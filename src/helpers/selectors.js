fb06f50e7420063e6ab644932106303f01fa3f9f
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-12-10 01:22:16 -0500

    add function to filter appointments by day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,10 @@
+export function getAppointmentsForDay(state, day) {
+  const filterDays= state.days.find((item)=> item.name === day)
+  if(!filterDays){
+    return []
+  }
+  const appointments = filterDays.appointments.map((item) => state.appointments[item])
+  return appointments; 
+}
+
+

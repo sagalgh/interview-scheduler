c12c2edea12c3b8f3681eb03032e011e2cdb16e9
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-12-13 00:04:57 -0500

    using function to conditionally render components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
-
 const Appointment = (props) => {
- 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
-    <Header time={props.time}/>
-   { props.interview ? 
-   <Show 
-   interviewer={props.interview.interviewer} 
-   onEdit={props.onEdit} 
-   onDelete={props.onDelete}
-   student={props.interview.student}
-   /> : <Empty  /> }
-    </article>
-  
-  )
-}
+      <Header time={props.time} />
+      {mode === SHOW && (
+        <Show
+          interviewer={props.interview.interviewer}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+          student={props.interview.student}
+        />
+      )}
 
-export default Appointment
+      {mode === EMPTY && <Empty />}
+    </article>
+  );
+};
+
+export default Appointment;
